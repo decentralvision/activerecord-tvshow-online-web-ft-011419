411af97c6f2cf724e298a2546733f9d0de7be62c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-02-22 19:13:38 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,19 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    binding.pry
+    Show.maximum(:rating)
+    binding.pry
+  end
+  def most_popular_show
+  end
+  def lowest_rating
+  end
+  def least_popular_show
+  end
+  def ratings_sum
+  end
+  def popular_shows
+  end
+  def shows_by_alphabetical_order
+  end
+end
